e5584151fc52b78b182b5701ebb0eaf438c3fbfe
Katarina Tuttle <katka.karolcikova@gmail.com>
2016-08-30 13:27:17 -0700

    getting XML response

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,13 @@ import config from '../config/environment';
 export default Ember.Route.extend({
   model: function() {
     var key = config.zillowApiKey;
-    var url = 'http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm?zws-id=' +key+ '&zpid=48749425&output=json&callback=cb';
-    return Ember.$.get(url).then(function(responseJSON) {
-      console.log(responseJSON);
+    // var qdlKey = config.quandlKey;
+    var url = 'http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm?zws-id=' +key+ '&zpid=48749425';
+    // var url = 'http://www.quandl.com/api/v3/datasets/ZILL/Z90210_A.json?api_key=UqoADriMdaicfBtMmP2r';
+    return Ember.$.get(url).then(function(response) {
+      console.log(response);
       // console.log(responseJSON.results); no results in xml only in JSON
-      return responseJSON.results;
+      return response;
     });
   }
 });
