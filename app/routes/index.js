fbbafcec7acdae2c36b4fecfc0bdaf6018b25a2e
Katarina Tuttle and Rony Nasr <katarina+rony@>
2016-08-31 11:31:50 -0700

    browsing xml response

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,24 +3,14 @@ import config from '../config/environment';
 // import Zillow from 'node-zillow';
 
 export default Ember.Route.extend({
-  // var Zillow = require('node-zillow');
+
   model: function() {
-    var key = config.zillowApiKey;
-    // var zillow = new Zillow(key);
-    // var parameters = {
-    //   zpid: 48749425
-    // };
-    // zillow.GetUpdatedPropertyDetails(parameters)
-    //   .then(function(results){
-    //     console.log(results);
-    //     return(results);
-    // });
-
-
-
-
-    var url = 'http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm?zws-id=' +key+ '&zpid=48749425';
+    // var key = config.zillowApiKey;
+    var key = "X1-ZWz19ldhyjtgqz_22g4o";
+    var url = 'http://www.zillow.com/webservice/GetDeepSearchResults.htm?citystatezip=Portland%2C+OR&address=2889+SE+Sherman&zws-id='+ key;
+    //var url = 'http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm?zws-id=' +key+ '&zpid=48749425';
     return Ember.$.get(url).then(function(response) {
+      debugger;
       console.log(response);
       return response;
     });
